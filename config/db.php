66f3b1ad330e595442f8252bcf60d07d8e4d9fa6
up2110h <qudratullayev97@gmail.com>
2020-12-05 21:25:28 +0500

    Добавлены модель таблицы currency и тесты на неё

--- config/db.php
+++ config/db.php
@@ -1,5 +1,8 @@
 <?php
 
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . "/..");
+$dotenv->load();
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => getenv('DB_DSN'),
---
+++ models/Currency.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "currency".
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $rate
+ * @property string $insert_dt
+ */
+class Currency extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'currency';
+    }
+
+    /**
+     * Устанавливает время записи
+     *
+     * @return array|array[]
+     */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['insert_dt'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['insert_dt'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'rate'], 'required'],
+            [['rate'], 'number'],
+            [['insert_dt'], 'safe'],
+            [['name'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'Идентификатор',
+            'name' => 'Название',
+            'rate' => 'Курс валюты к рублю',
+            'insert_dt' => 'Время обновления валюты',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return CurrencyQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CurrencyQuery(get_called_class());
+    }
+}
